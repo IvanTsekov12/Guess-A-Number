e15ba61a77b4e70097e1c24b96d9aaea3345b0b0
Ivan Tsekov <ivantsekov16@gmail.com>
2023-05-20 15:24:20 +0300

    Number guessing game created

---
+++ guessANumber/guessANumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace guessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 101);
+            Console.Write("Guess a number (1-100): ");
+            int myNumber = int.Parse(Console.ReadLine());
+
+            while (myNumber != number)
+            {
+                if (myNumber > number)
+                {
+                    Console.WriteLine("Too High!");
+                }
+                else if (myNumber < number)
+                {
+                    Console.WriteLine("Too Low!");
+                }
+                Console.Write("Guess a number (1-100): ");
+                myNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
